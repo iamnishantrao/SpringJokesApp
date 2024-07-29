3f6d64a4a1d78d0a9d97081c927d976f413b0626
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-29 03:14:05 -0700

    Create Joke Controller and View

---
+++ src/main/java/org/example/springjokesapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package org.example.springjokesapp.controllers;
+
+import org.example.springjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+        return "joke";
+    }
+}
